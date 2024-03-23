2e9c3c987e708a860d5999623e79bd14c65ffe11
Neomar35 <143468277+Neomar35@users.noreply.github.com>
2024-03-23 07:43:09 -0400

    Update anchodepulso.h

--- anchodepulso.h
+++ anchodepulso.h
@@ -11,8 +11,9 @@ const int MIN_ANCHO_PULSO = 0 ;
 const int MAX_ANCHO_PULSO = 17;
 const float TEMPERATURA_ARRANQUE = 0.0; // Temperatura de arranque en °C
 const float TEMPERATURA_APAGADO = 60.0; // Temperatura de apagado en °C
-const float PORCENTAJE_COMBUSTIBLE_EXTRA = 0.2; // Porcentaje de combustible extra (20%)
-
+extern const float PORCENTAJE_COMBUSTIBLE_EXTRA;// Porcentaje de combustible extra (%)
+extern int usarSensorO2; 
+extern float ajusteO2Global;
 
    float calcularAnchoDePulso(float valorSensorMap, float valorSensorO2, float temperature) {
   // Calcular el ancho de pulso en función del valor invertido del sensor MAP/TPS
@@ -23,27 +24,26 @@ const float PORCENTAJE_COMBUSTIBLE_EXTRA = 0.2; // Porcentaje de combustible ext
 
   // Verificar si la temperatura está dentro del rango de arranque
   if (temperature < TEMPERATURA_APAGADO) {
-    // Si la temperatura es menor a 60°C, agregar un 20% al ancho de pulso
-    float porcentajeExtra = 0.2;
-    delayTime += delayTime * porcentajeExtra;
+    // Si la temperatura es menor a 60°C, agregar un 20% al ancho de pulso  k
+    delayTime += delayTime * PORCENTAJE_COMBUSTIBLE_EXTRA;
     // Serial.println("Porcentaje Extra: " + String(porcentajeExtra));
    // Serial.println("Delay Time después de ajustar por temperatura: " + String(delayTime));
   }
 
 
   // Verificar si se debe usar el sensor de oxígeno en el cálculo del ancho de pulso
-  if (valorSensorO2 != 0) {
+  if (usarSensorO2 != 0 && valorSensorO2 != 0) {
     // Si el valor del sensor de oxígeno no es cero, ajustar el ancho de pulso en función de su valor
     float voltageO2 = mapFloat(valorSensorO2, 0.0, 1023.0, 0.0, 5000.0) / 1000.0;
     float ajusteO2 = voltageO2 - 2.5;
 
     if (ajusteO2 < 0) {
       // Si el ajuste es negativo, aumentar el ancho de pulso
-      delayTime += mapFloat(ajusteO2, -2.5, 0.0, 0.2, 1.0);
+      delayTime += mapFloat(ajusteO2, -2.5, 0.0, 0.2, ajusteO2Global);
       // Serial.println("Delay Time después de aumentar por ajuste O2 negativo: " + String(delayTime));
     } else {
       // Si el ajuste es positivo, disminuir el ancho de pulso
-      delayTime -= mapFloat(ajusteO2, 0.0, 2.5, 0.2, 1.0);
+      delayTime -= mapFloat(ajusteO2, 0.0, 2.5, 0.2, ajusteO2Global);
       // Serial.println("Delay Time después de disminuir por ajuste O2 positivo: " + String(delayTime));
     }
   }
